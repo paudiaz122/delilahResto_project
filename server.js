87b0ee009539a543c5243f1817449d507434ddbf
paudiaz122 <pau.diaz@hotmail.com.ar>
2020-07-15 20:13:30 -0300

    server folder deleted

--- server.js
+++ server.js
@@ -8,20 +8,12 @@ const ordersRoutes = require('./routes/orders_routes');
 const productsRoutes = require('./routes/products_routes');
 const usersRoutes = require('./routes/users_routes');
 
+server.use(bodyParser.json());
+
 //Para cada petición que se haga a los siguientes endpoints, uso las rutas específicas
 server.use('/orders', ordersRoutes);
 server.use('/products', productsRoutes);
 server.use('/users', usersRoutes);
-
-/** Middlewares */
-server.use(bodyParser.json());
-
-// server.use((err, req, res, next) => {
-//     if(!err) return next();
-
-//     console.log('Error, algo salió mal', err);
-//     res.status(500).send('Error');
-// });
 
 /** Server */
 server.listen(3000, () => {
